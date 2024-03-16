80d0430c69aa280f3e12635c1a8531071590dc3b
jacjjao <hjacob1821@gmail.com>
2024-03-16 09:23:59 +0800

    fix

--- project1/jiffies.c
+++ project1/jiffies.c
@@ -11,19 +11,18 @@
 #endif
 
 #define PROCFS_NAME "jiffies"
+#define SIZE 50
 
-/* This structure hold information about the /proc file */
-
-static struct proc_dir_entry *our_proc_file;
+static struct proc_dir_entry *proc_file;
 
 static ssize_t procfile_read(struct file *file_pointer, char __user *buffer,
                              size_t buffer_length, loff_t *offset) {
   unsigned long j;
-  char buf[100];
+  char buf[SIZE];
   int len;
 
   j = jiffies;
-  len = snprintf(buf, 100, "%lu\n", j);
+  len = snprintf(buf, SIZE, "%lu\n", j);
 
   if (*offset >= len || copy_to_user(buffer, buf, len)) {
     pr_info("copy_to_user failed\n");
@@ -50,9 +49,9 @@ static const struct file_operations proc_file_fops = {
 #endif
 
 static int __init procfs2_init(void) {
-  our_proc_file = proc_create(PROCFS_NAME, 0644, NULL, &proc_file_fops);
+  proc_file = proc_create(PROCFS_NAME, 0644, NULL, &proc_file_fops);
 
-  if (NULL == our_proc_file) {
+  if (NULL == proc_file) {
     pr_alert("Error:Could not initialize /proc/%s\n", PROCFS_NAME);
     return -ENOMEM;
   }
@@ -63,7 +62,7 @@ static int __init procfs2_init(void) {
 }
 
 static void __exit procfs2_exit(void) {
-  proc_remove(our_proc_file);
+  proc_remove(proc_file);
 
   pr_info("/proc/%s removed\n", PROCFS_NAME);
 }
