390942bfb197eae4d0d300367d35bfbd45f81fd0
jacjjao <hjacob1821@gmail.com>
2024-03-16 15:06:13 +0800

    update

--- hw1/file_copy.c
+++ hw1/file_copy.c
@@ -3,60 +3,6 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int oshw_file_cpy(const char *src, const char *dest) {
-  int fd_to, fd_from;
-  char buf[4096];
-  ssize_t nread;
-  int saved_errno;
-
-  fd_from = open(src, O_RDONLY);
-  if (fd_from < 0) {
-    fprintf(stderr, "Cannot open the src file\n");
-    return -1;
-  }
-
-  mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
-  fd_to = open(dest, O_WRONLY | O_CREAT | O_TRUNC, mode);
-  if (fd_to < 0) {
-    fprintf(stderr, "Cannot open the dest file\n");
-    goto out_error;
-  }
-
-  while (nread = read(fd_from, buf, sizeof buf), nread > 0) {
-    char *out_ptr = buf;
-    ssize_t nwritten;
-
-    do {
-      nwritten = write(fd_to, out_ptr, nread);
-
-      if (nwritten >= 0) {
-        nread -= nwritten;
-        out_ptr += nwritten;
-      } else if (errno != EINTR) {
-        fprintf(stderr, "Error while writing the file\n");
-        goto out_error;
-      }
-    } while (nread > 0);
-  }
-
-  if (nread == 0) {
-    if (close(fd_to) < 0) {
-      fd_to = -1;
-      fprintf(stderr, "Cannot close the dest file\n");
-      goto out_error;
-    }
-    close(fd_from);
-
-    return 0;
-  }
-
-out_error:
-  close(fd_from);
-  if (fd_to >= 0) close(fd_to);
-
-  return -1;
-}
-
 int main(void) {
   char src_fname[100];
   char dest_fname[100];
@@ -67,10 +13,53 @@ int main(void) {
   printf("The file's name you want to copy to: ");
   scanf("%99s", dest_fname);
 
-  if (oshw_file_cpy(src_fname, dest_fname)) {
-    printf("error while copy the file\n");
+  int fd_to, fd_from;
+  char buf[4096];
+  ssize_t nread;
+  int ret;
+
+  ret = 0;
+
+  fd_from = open(src_fname, O_RDONLY);
+  if (fd_from < 0) {
+    fprintf(stderr, "Cannot open the src file\n");
     return -1;
   }
+
+  fd_to = open(dest_fname, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+  if (fd_to < 0) {
+    fprintf(stderr, "Cannot open the dest file\n");
+    ret = -1;
+    goto cleanup;
+  }
+
+  while (nread = read(fd_from, buf, sizeof(buf)), nread > 0) {
+    char *out_ptr = buf;
+    ssize_t nwritten;
+
+    do {
+      nwritten = write(fd_to, out_ptr, nread);
+
+      if (nwritten >= 0) {
+        nread -= nwritten;
+        out_ptr += nwritten;
+      } else if (nwritten < 0 && errno != EINTR) {
+        fprintf(stderr, "Error while writing the file\n");
+        ret = -1;
+        goto cleanup;
+      }
+    } while (nread > 0);
+  }
+
+  if (nread < 0) {
+    ret = -1;
+  }
+
+cleanup:
+  close(fd_from);
+  if (fd_to >= 0) close(fd_to);
+
   printf("finish\n");
-  return 0;
+
+  return ret;
 }
